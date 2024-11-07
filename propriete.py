7742649181aac9e02f52b30425905e3eb6866080
Shuucroute <ethan.rampnoux@gmail.com>
2024-11-07 10:37:29 +0100

    Suite

--- propriete.py
+++ propriete.py
@@ -1,3 +1,14 @@
-class Proppriete:
-    def __init__(self):
-        self.prix = []
+class Propriete:
+    def __init__(self, prixAchat, nom):
+        self.prixAchat = prixAchat
+        self.nom = nom
+        self.hypotheque = False
+        #self.prixHypotheque = self.prixAchat / 2
+
+
+    def __str__(self):
+        return (f"{self.nom} : ({self.prixAchat} €)")
+
+ruePaix = Propriete(400, "Rue de la paix")
+print(ruePaix)
+        
