f41f34bcec451da5964ae37c125ea988fd8f3057
xMokiii <morlioncolin@gmail.com>
2024-11-08 10:18:15 +0100

    modif gare

--- gare.py
+++ gare.py
@@ -1,24 +1,21 @@
 from propriete import Propriete
 
 class Gare(Propriete):
-    def __init__(self,prixAchat,nom):
-        super().__init__(prixAchat, nom)
-        self.__loyers = [25,50,100,200]
-        self.__nbGares = 0
+    loyers = [25,50,100,200]
+    prixAchatGare = 200
+    def __init__(self,nom):
+        super().__init__(nom,Gare.prixAchatGare)
 
-    def calculerLoyer(self):
-        return self.__loyers[self.nbGares]
-
-    @property
-    def nbGares(self):
-        return self.__nbGares
-
-    @nbGares.setter
-    def nbGares(self,valeur):
-        self.__nbGares = valeur
+    def calculerLoyer(self):    
+        return self.loyers[self.nbGares]
 
 if __name__ == '__main__':
-    gareMontparnasse = Gare(200, "Gare Montparnasse")
+    gareMontparnasse = Gare("Gare Montparnasse")
     print(gareMontparnasse)
-    gareMontparnasse.nbGares = 1
+    gareMontparnasse.nbGares += 1
     print(f"Le loyer est de {gareMontparnasse.calculerLoyer()}") 
+    gareSaintLazare = Gare("Gare Saint-Lazare")
+    print(gareSaintLazare)
+    gareSaintLazare.nbGares += 1
+    print(f"Le loyer est de {gareSaintLazare.calculerLoyer()}") 
+
