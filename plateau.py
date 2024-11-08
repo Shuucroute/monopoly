bdc6fe94a79feb3e3ca03c46ae4b93c93c301204
xMokiii <morlioncolin@gmail.com>
2024-11-08 09:03:29 +0100

    terrain

--- plateau.py
+++ plateau.py
@@ -1,6 +1,25 @@
-from propriete import *
+from propriete import Propriete
 class Terrain (Propriete):
     def __init__ (self, prixAchat, nom, loyers):
         super().__init__(prixAchat, nom)
-        self.loyers = loyers
-        self.__nbMaisonss = 0
+        self.__loyers = loyers
+        self.__nbMaisons = 0
+
+    def calculerLoyer(self):
+        return self.__loyers[self.nbMaisons]
+
+    @property
+    def nbMaisons(self):
+        return self.__nbMaisons
+
+    @nbMaisons.setter
+    def nbMaisons(self,valeur=None):
+        if valeur <5:
+            self.__nbMaisons = valeur
+
+if __name__ == '__main__':
+    rueDeLaPaix = Terrain(400, "Rue de la paix", [50,200,600,1400,1700,2000])
+    
+    print(rueDeLaPaix)
+    rueDeLaPaix.nbMaisons = 2
+    print(f"Le loyer est de {rueDeLaPaix.calculerLoyer()}") 
