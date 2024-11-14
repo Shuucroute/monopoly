f46eb77b4e4f81969081063f15571e9dc598e34c
Shuucroute <ethan.rampnoux@gmail.com>
2024-11-14 09:06:06 +0100

    ouioui

--- ouioui.py
+++ ouioui.py
@@ -4,9 +4,20 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="password",
+    database = "monopoly",
 )
 
 print(mydb)
 
 monCurseur = mydb.cursor()
-monCurseur.execute("CREATE")
+monCurseur.execute("""
+CREATE TABLE quartiers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    couleur VARCHAR(255),
+    prixMaison INT,
+    CONSTRAINT PK_quartiers PRIMARY KEY (id)
+);
+""")
+
+print("")
